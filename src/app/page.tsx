d083ad90a887e20cb601109c3dfb418d994541f0
Venkataramana hegde <venkataramanahegde64@gmail.com>
2025-05-05 23:58:49 +0530

    Add clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+"use client"
+
+import { SignedIn, SignOutButton } from '@clerk/clerk-react'
+import { SignedOut, SignInButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      HomePage
+
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
   )
 }
 
